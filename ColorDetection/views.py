aac50f5820e42ea3c3493926fed76bf335513475
root <root@ip-172-31-16-248.ec2.internal>
2022-03-28 22:02:57 +0000

    draft changes to support images

--- ColorDetection/views.py
+++ ColorDetection/views.py
@@ -15,7 +15,11 @@ logging.basicConfig(level=logging.DEBUG)
 @csrf_exempt
 @api_view(["POST"])
 def process_image(request):
+    # logger.debug(f"{request.__dict__}")
     logger.debug(f"received request : {request.data}")
+    image_file = request.data.get('data').file
+    logger.debug(f"received image : {type(image_file)}")
+    logger.debug(f"received request : {image_file.read()}")
     response_body = {'status code': HTTP_200_OK,
                      'body': 'dummy response',
                      }
