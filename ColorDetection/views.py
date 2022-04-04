a4785c527b7560bd2c3a36959f4bcd146f14a5d3
Rakesh N <ranair@tcd.ie>
2022-04-04 18:19:45 +0100

    updated k-mean logic for color-detection.

--- ColorDetection/views.py
+++ ColorDetection/views.py
@@ -1,22 +1,21 @@
+import datetime
+import io
+import logging
+import random
+# import mysql.connector
+# from mysql.connector import errorcode
+# from sqlalchemy import create_engine
+import sys
+import time
+from math import sqrt
+
+from PIL import Image
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.status import (
-    HTTP_400_BAD_REQUEST,
     HTTP_200_OK)
-import logging
-import io
-import cv2
-import numpy as np
-from PIL import Image
-import mysql.connector
-from mysql.connector import errorcode
-from sqlalchemy import create_engine
-import time, sys
-import datetime
-import cv2
 from sklearn.cluster import KMeans
-from collections import Counter
 
 # Create your views here.
 
@@ -29,24 +28,106 @@ container_map = {
     3: "Red"
 }
 
-def get_image(img):
-    arr_img = np.asarray(img)
-    rgb_img = cv2.cvtColor(arr_img, cv2.COLOR_BGR2RGB)
-    return rgb_img
+
+class Point:
+    def __init__(self, coordinates):
+        self.coordinates = coordinates
 
 
-def get_colours(image, max_value):
-    img = cv2.resize(image, (600, 400))
-    img = img.reshape(img.shape[0] * img.shape[1], 3)
-    cluster = KMeans(n_clusters=max_value)
-    labels = cluster.fit_predict(img)
-    ct = Counter(labels)
-    center = cluster.cluster_centers_
-    order = [center[i] for i in ct.keys()]
-    top_detected_color = [rgb.tolist() for rgb in order]
-    for color in top_detected_color:
-        logger.info(f"[+] {color}")
-    return top_detected_color
+class Cluster:
+    def __init__(self, center, points):
+        self.center = center
+        self.points = points
+
+
+class KMeans:
+
+    def __init__(self, n_clusters, min_diff=1):
+        self.n_clusters = n_clusters
+        self.min_diff = min_diff
+
+    def calculate_center(self, points):
+        n_dim = len(points[0].coordinates)
+        vals = [0.0 for i in range(n_dim)]
+        for p in points:
+            for i in range(n_dim):
+                vals[i] += p.coordinates[i]
+        coords = [(v / len(points)) for v in vals]
+        return Point(coords)
+
+    def assign_points(self, clusters, points):
+        plists = [[] for i in range(self.n_clusters)]
+
+        for p in points:
+            smallest_distance = float('inf')
+
+            for i in range(self.n_clusters):
+                distance = euclidean(p, clusters[i].center)
+                if distance < smallest_distance:
+                    smallest_distance = distance
+                    idx = i
+
+            plists[idx].append(p)
+
+        return plists
+
+    def fit(self, points):
+        clusters = [Cluster(center=p, points=[p]) for p in random.sample(points, self.n_clusters)]
+
+        while True:
+
+            plists = self.assign_points(clusters, points)
+
+            diff = 0
+
+            for i in range(self.n_clusters):
+                if not plists[i]:
+                    continue
+                old = clusters[i]
+                center = self.calculate_center(plists[i])
+                new = Cluster(center, plists[i])
+                clusters[i] = new
+                diff = max(diff, euclidean(old.center, new.center))
+
+            if diff < self.min_diff:
+                break
+
+        return clusters
+
+
+def euclidean(p, q):
+    n_dim = len(p.coordinates)
+    return sqrt(sum([
+        (p.coordinates[i] - q.coordinates[i]) ** 2 for i in range(n_dim)
+    ]))
+
+
+def get_points(image_path):
+    img = Image.open(image_path)
+    img.thumbnail((200, 400))
+    img = img.convert("RGB")
+    w, h = img.size
+
+    points = []
+    for count, color in img.getcolors(w * h):
+        for _ in range(count):
+            points.append(Point(color))
+
+    return points
+
+
+def rgb_to_hex(rgb):
+    return '#%s' % ''.join(('%02x' % p for p in rgb))
+
+
+def get_colors(filename, n_colors=3):
+    points = get_points(filename)
+    clusters = KMeans(n_clusters=n_colors).fit(points)
+    clusters.sort(key=lambda c: len(c.points), reverse=True)
+    rgbs = []
+    for c in clusters:
+        rgbs.append(c.center.coordinates)
+    return rgbs
 
 
 @csrf_exempt
@@ -56,13 +137,12 @@ def process_image(request):
     db_name = "sorting_inventory"
     table_name = "packages_inventory"
     max_allowed_color = 5
-    img = Image.open(io.BytesIO(request.body))
-    rgb_img = get_image(img)
-    color_detected = get_colours(rgb_img, max_allowed_color)
+    buff = io.BytesIO(request.body)
+    color_detected = get_colors(buff, n_colors=5)
     final_color = max([color.index(max(color)) for color in color_detected])
-    color_code = container_map[final_color + 1]
+    color_code = final_color + 1
     logger.info(f"{final_color} - {color_code}")
-    flag = insert_records(db_name, table_name, color_code)
+    # flag = insert_records(db_name, table_name, container_map[color_code])
     return Response(color_code, status=HTTP_200_OK)
 
 
